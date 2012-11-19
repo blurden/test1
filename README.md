3b1d4ee311a8f297ebc9e1554325e25d11787031
blurden <blurden@gmail.com>
2012-11-18 22:38:03 -0800

    readme update

---
+++ README.md
@@ -0,0 +1,4 @@
+== This is a test
+
+Rails is a web-application framework that includes everything needed to create
+database-backed web applications according to the Model-View-Control pattern.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Test1::Application.config.secret_token = '9279d4731b3d9c3c2022a307e979603922093d6ed9df1ee8c751d2100ce81ad3b8f1f0aaae6ba45fe1d6f295cc8fc13d783f24478ca98c5efc8d4f769e3c5c95'
